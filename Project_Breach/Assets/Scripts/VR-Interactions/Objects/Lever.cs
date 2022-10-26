efb69291d855481c870f3a550eebacd9c1f36bd4
AdamHTang <56270909+AdamHTang@users.noreply.github.com>
2022-10-25 23:26:37 -0500

    Lever
    Added a lever prefab and script.
    Lever can trigger a Unity event and locks position once it is pulled.

---
+++ Project_Breach/Assets/Scripts/VR-Interactions/Objects/Lever.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Lever : MonoBehaviour
+{
+    [Header("Lever Attributes")]
+    public HingeJoint hinge;
+    public float bounds;
+    public float damper;
+    [Range(0.0f, 1.0f)] public float threshhold; 
+    public UnityEvent onLeverPull;
+    public XRGrabInteractable handleInteractable;
+    public Rigidbody rb;
+
+    [SerializeField] private bool isPulled;
+    
+
+     void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        handleInteractable = GetComponent<XRGrabInteractable>();
+        rb = GetComponent<Rigidbody>();
+        JointLimits limits = hinge.limits;
+        JointSpring spring = hinge.spring;
+        bounds = Mathf.Abs(bounds);
+        limits.min = -bounds;
+        limits.max = bounds;
+        spring.damper = damper;
+        hinge.limits = limits;
+        hinge.spring = spring;
+        transform.rotation = new Quaternion(0.0f, transform.rotation.y, transform.rotation.z, 0.0f);
+        isPulled = false;
+    }
+
+     void Update()
+    {
+        
+        if(transform.localRotation.x >= threshhold)
+        {
+            isPulled = true;
+            onLeverPull.Invoke();
+            handleInteractable.enabled = false;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            this.enabled = false;
+        }
+
+    }
+}
