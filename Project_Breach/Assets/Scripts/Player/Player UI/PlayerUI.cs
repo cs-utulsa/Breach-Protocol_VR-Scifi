aa0a6feda4b7bd3d08b5e6516421afe16268348e
AdamHTang <56270909+AdamHTang@users.noreply.github.com>
2022-10-27 17:44:25 -0500

    TMPro and Socket UI
    Imported the TMPro package.
    Edited the PlayerUI script to tell the player if they have a primary or secondary weapon in their inventory.

--- Project_Breach/Assets/Scripts/Player/Player UI/PlayerUI.cs
+++ Project_Breach/Assets/Scripts/Player/Player UI/PlayerUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,19 +13,54 @@ public class PlayerUI : MonoBehaviour
     [Header("UI Elements")]
     public Slider healthSlider;
     public Slider shieldSlider;
+    public TextMeshProUGUI primaryText;
+    public TextMeshProUGUI secondaryText;
 
+    [Header("Inventory Sockets")]
+    public SocketTagCheck primarySocket;
+    public SocketTagCheck secondarySocket;
 
+    private bool primaryFull;
+    private bool secondaryFull;
     private void Start()
     {
         playerHealth = GetComponentInParent<Health>();
         playerShield = GetComponentInParent<Shield>();
         healthSlider.maxValue = playerHealth.playerData.maxHealth;
         shieldSlider.maxValue = playerShield.playerData.maxShield;
+        primaryFull = false;
+        secondaryFull = false;
     }
 
     private void Update()
     {
         shieldSlider.value = playerShield.getShieldCharge();
         healthSlider.value = playerHealth.getCurrentHealth();
+    }
+
+    public void TogglePrimaryUI()
+    {
+        primaryFull = !primaryFull;
+        if (primaryFull)
+        {
+            primaryText.text = "You have a primary.";
+        }
+        else
+        {
+            primaryText.text = "No primary.";
+        }
+    }
+
+    public void ToggleSecondaryUI()
+    {
+        secondaryFull = !secondaryFull;
+        if (secondaryFull)
+        {
+            secondaryText.text = "You have a secondary.";
+        }
+        else
+        {
+            secondaryText.text = "No secondary.";
+        }
     }
 }
